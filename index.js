3c0e688855c35a324a67c89bbc336780db8534bf
Gabriel <gabriel.boeira@softplan.com.br>
2022-08-05 22:34:50 -0300

    Compra e venda MARKET

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//index.js
 const api = require('./api');
 const symbol = process.env.SYMBOL;
-const profitability = process.env.PROFITABILITY;
+const profitability = parseFloat(process.env.PROFITABILITY);
 
 console.log('Iniciando monitoramento!');
 setInterval(async () => {
@@ -14,98 +13,57 @@ setInterval(async () => {
     console.log(mercado.asks.length ? `Venda (Menor asks): ${mercado.asks[0][0]}` : 'Sem Vendas'); //Menor preço de vendas
 
     mercado.bids ? buy = parseInt(mercado.bids[0][0]) : buy = 0;
-    mercado.asks ? buy = parseInt(mercado.asks[0][0]) : asks = 0;
+    mercado.asks ? sell = parseInt(mercado.asks[0][0]) : asks = 0;
+
+    console.log('*-*-*-*-*-*-*-*-*-*-*-*-*');
+    console.log(sell);
+    console.log('*-*-*-*-*-*-*-*-*-*-*-*-*');
 
 
-    // // COMPRAR  
-    // if (sell < 290.40) {
+    //--COMPRAR  
+    if (sell < 400.40) {
 
-    //     console.log('Consultar carteira - Bom de comprar');
-    //     const carteira = await api.accountInfo();
-    //     const coins = carteira.balances;
-    //     // const coins = carteira.balances.filter(b => b.asset === 'BUSD' || b.asset === 'BNBBUSD');
-    //     console.log('--------------------------------------');
-    //     console.log(coins);
-    //     console.log('--------------------------------------');
-       
-    //     console.log('Verificando se tenho grana...');
-    //     const totalCoin = parseInt(coins.find(c => c.asset === 'BUSD').free);
-    //     console.log(`Total Coin:  ${totalCoin}}`);
-
-    //     if (sell <= totalCoin) {
-    //         // console.log(await api.newOrder(symbol, 1)) //situação geral da operação
-
-    //         // const buyOrder = await api.newOrder(symbol, 1)
-    //         // console.log(`orderId: ${buyOrder.orderId}`)
-    //         // console.log(`status: ${buyOrder.status}`)
-            
-    //         // console.log('Posicionando venda futura!!!');
-    //         // const price = parseInt(sell * profitability);
+        console.log('Consultar carteira - Bom de comprar');
+        const carteira = await api.accountInfo();
+        const coins = carteira.balances;
+        //--const coins = carteira.balances.filter(b => b.asset === 'BUSD' || b.asset === 'BNBBUSD');
+        // console.log('----------------Resultado da carteira----------------');
+        // console.log(coins);
+        // console.log('-----------------------------------------------------');
 
 
-    //         // console.log(`Vendendo por ${price} (${profitability})`)
-    //         // const sellOrder = await api.newOrder(symbol, 1, price, 'SELL', 'LIMIT');
-    //         // console.log(`orderId: ${sellOrder.orderId}`)
-    //         // console.log(`status: ${sellOrder.status}`)
-    //     }
+        ////--POSICIONANDO COMPRA
+        console.log('Verificando se tenho grana...');
+        const totalCoin = parseInt(coins.find(c => c.asset === 'BUSD').free);
+        console.log(`Total Coin:  ${totalCoin}}`);
 
-    // // VENDER
-    // } else if (buy > 1000) {
-    //     console.log('Bom para vender');
-    
-    // // AGUARDANDO MERCADO
-    // } else {
-    //     console.log('Aguardando mercado');
-    // }
+        if (sell <= totalCoin) {
+            // //--Ordem de compra: console.log(await api.newOrder(symbol, 1)) //situação geral da operação
+            // const buyOrder = await api.newOrder(symbol, 1)
+            // console.log(`orderId: ${buyOrder.orderId}`)
+            // console.log(`status: ${buyOrder.status}`)
 
+
+            ////--POSICIONANDO VENDA
+            // console.log('Posicionando venda futura!!!');
+            // const price = parseInt(sell * profitability);
+            // console.log(`Vendendo por ${price} (${profitability})`)
+            // const sellOrder = await api.newOrder(symbol, 1, price, 'SELL', 'MARKET');
+            // console.log(`orderId: ${sellOrder.orderId}`)
+            // console.log(`status: ${sellOrder.status}`)
+        }
+
+        console.log('----------------Resultado da carteira----------------');
+        console.log(coins);
+        console.log('-----------------------------------------------------');
+
+        //--VENDER
+    } else if (buy > 1000) {
+        console.log('Bom para vender');
+
+        //--AGUARDANDO MERCADO
+    } else {
+        console.log('Aguardando mercado');
+    }
 
 }, process.env.CRAWLER_INTERVAL);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const api = require('./api');
-// const symbol = process.env.SYMBOL;
-
-// setInterval(async () => {
-
-//     let buy = 0, sell = 0;
-
-//     const result = await api.depth('BNBBUSD');
-
-//     if (result.bids) {
-//         console.log(`Highest buy (Maior preco de compras): ${result.bids[0][0]}`); //Maior preço de compras
-//         const buy = parseInt(result.bids[0][0]);
-//     }
-
-//     if (result.asks) {
-//         console.log(`Lowest sell (Menor preco de vendas): ${result.asks[0][0]}`); //Menor preço de vendas
-//         const sell = parseInt(result.asks[0][0]);
-//     }
-
-
-//     // console.log(await api.accountInfo());
-
-
-//     // if (sell < 700) {
-//     //     //Bom para comprar
-//     // } else if (buy > 1000) {
-//     //     //Bom para vender
-//     // } else {
-//     //     //Aguardando mercado
-//     // }
-
-
-// }, process.env.CRAWLER_INTERVAL);
-
-// // bids => O que as pessoas querem pagar = Ordem de compra
-// // asks => O que as pessoas querem vender = Ordem de venda
